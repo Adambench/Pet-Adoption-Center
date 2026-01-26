5d0ae46597d94b4df5c1638dd617c6ff8dc3e690
Adambench <166659670+Adambench@users.noreply.github.com>
2026-01-25 19:32:56 -0500

    Refactor App component: remove unused code and integrate Header component

---
+++ src/Components/Header.jsx
@@ -0,0 +1,18 @@
+
+
+function Header(){
+
+
+    return (<header>
+                <div>
+                    <h1>
+                        Any Name
+                    </h1>
+                    <img src="https://static.vecteezy.com/system/resources/previews/036/405/679/large_2x/free-cute-cat-mascot-logo-design-free-vector.jpg" alt="" />
+                </div>
+
+            </header>);
+}
+
+
+export default Header
