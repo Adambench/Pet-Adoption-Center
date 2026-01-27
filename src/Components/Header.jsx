8590bcc69e249eada9458a0b8db6fedc43e08994
Adambench <166659670+Adambench@users.noreply.github.com>
2026-01-27 13:12:06 -0500

    header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,11 +4,31 @@ function Header(){
 
 
     return (<header>
-                <div>
-                    <h1>
-                        Any Name
-                    </h1>
+                <div className="header">
+                    <a href="">
                     <img src="https://static.vecteezy.com/system/resources/previews/036/405/679/large_2x/free-cute-cat-mascot-logo-design-free-vector.jpg" alt="" />
+                    </a>
+
+                    <h1>
+                        XX
+                    </h1>
+
+                    <nav>
+                        <ul>
+                            <li>
+                                <a href="">Home</a>
+                            </li>
+                            <li>
+                                <a href="">Sign up</a>
+                            </li>
+                            <li>
+                                <a href="">Log in</a>
+                            </li>
+                            <li>
+                                <a href="">About us</a>
+                            </li>
+                        </ul>
+                    </nav>
                 </div>
 
             </header>);
